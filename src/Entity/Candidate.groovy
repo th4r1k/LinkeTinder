78862084edf6fadd22ca78f21208db2f5f63b9d6
th4r1k <th4r1k@gmail.com>
2023-01-29 07:06:20 -0300

    refactor: entity methods

--- src/Entity/Candidate.groovy
+++ src/Entity/Candidate.groovy
@@ -1,8 +1,82 @@
 package Entity
 
+import Data.Candidates
+import Data.Enterprises
+import Utils.Navigate
+
 class Candidate extends User {
 
     int age
     List<String> qualification
 
+
+    void create(String name, String email, int age, String country, int zipCode, String state, int doc, def qualification){
+        Candidate newCandidate = new Candidate(name: name, email: email, age: age, country: country, zipCode: zipCode, state: state, doc: doc, qualification: qualification)
+        Candidates.list += newCandidate
+    }
+
+
+    void like(String name, def id){
+        Scanner input = new Scanner(System.in);
+        println "0 - Like"
+        println "1 - Proximo"
+        String cmd = input.nextLine();
+        if (cmd == "0") {
+
+            for (int i = 0; i < Candidates.list.size(); i++) {
+                if (Candidates.list[i].name == name) {
+                    Candidates.list[i].likes += id
+
+                }
+            }
+        }
+    }
+
+
+    void match(String name){
+        boolean match = false
+        for (int i = 0; i < Candidates.list.size(); i++) {
+            if (Candidates.list[i].name == name) {
+
+                for (int j = 0; j < Enterprises.list.size(); j++) {
+                    for (int k = 0; k < Enterprises.list[j].opportunity.size(); k++) {
+                        if (Candidates.list[i].likes.flatten().contains(Enterprises.list[j].opportunity[k].id)) {
+                            if (Enterprises.list[j].likes.contains(name)) {
+                                println "***********************************************"
+                                println "Match com a empresa ${Enterprises.list[j].name}"
+                                println "***********************************************"
+                                match = true
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if(!match) {
+            println("")
+            println("Nenhum match ate o momento")}
+        Navigate.candidateMenu(name)
+    }
+
+    void getOpportunities(String name) {
+        Candidate candidate = new Candidate()
+        Enterprises.list.forEach {
+            if (it.opportunity) {
+                if (it.opportunity.size() > 1) {
+                    it.opportunity.forEach {
+
+                        candidate.like(name, it.id)
+                    }
+                } else {
+                    println it.opportunity
+                    candidate.like(name, it.opportunity.id)
+                }
+            } else {
+                println "Nao ha mais vagas"
+
+            }
+        }
+        Navigate.candidateMenu(name)
+    }
 }
