f78969b1a302d66791643924028b5bf7f1743d88
th4r1k <th4r1k@gmail.com>
2023-02-13 09:22:18 -0300

    feat: regex new users

--- Backend/src/Entity/User.groovy
+++ Backend/src/Entity/User.groovy
@@ -3,18 +3,11 @@ package Entity
 import groovy.transform.Canonical
 
 @Canonical
-abstract class User {
+ class User {
 
-<<<<<<< Updated upstream
-    String name, email, country, state
-    int doc, zipCode
-//    List<User> matches = []
-    List<User> likes = []
-=======
     String name, email, country, state, password, category
     BigInteger doc
     int zipCode
->>>>>>> Stashed changes
 
 
 }
